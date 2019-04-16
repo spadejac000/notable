1a0384d437d7899ca07e2ee0fa6fe10f9e740dc9
Jacob Spade <spade.jacob@Yahoo.com>
2019-04-15 20:08:37 -0700

    connected mlab database

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,14 @@
 module.exports = (app, db) => {
   app.post('/notes', (req, res) => {
-    // create note here
-    res.send('hello world');
+    const note = {text: req.body.body, title: req.body.title}
+    db.collection('notes').insert(note, (err, results) => {
+      if(err) {
+        res.send({
+          'error': 'an error happended'
+        })
+      } else {
+        res.send(result.ops[0])
+      }
+    })
   })
 }
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 
 const port = 5000;
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('we are live on port ' + port)
-})
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect(db.url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  } else {
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+      console.log('we are live on port ' + port)
+    })
+  }
+});
