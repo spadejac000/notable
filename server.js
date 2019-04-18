bd327a2b1e94eb2ef67da60835cf639f574a2e52
Jacob Spade <spade.jacob@Yahoo.com>
2019-04-18 10:49:13 -0700

    now able to add data to database

--- server.js
+++ server.js
@@ -7,28 +7,12 @@ const app = express();
 
 const port = 5000;
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
 MongoClient.connect(db.url, (err, database) => {
-  if(err) {
-    return console.log(err)
-  } else {
-    require('./app/routes')(app, database);
-    app.listen(port, () => {
-      console.log("we are live on port " + port);
-    })
-  }
-});
-
-app.post('/notes', (req, res) => {
-    const note = {text: req.body.body, title: req.body.title}
-    db.collection('notes').insert(note, (err, result) => {
-      if(err) {
-        res.send({
-          'error': 'an error happended'
-        })
-      } else {
-        res.send(result.ops[0])
-      }
-    })
+  if(err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log("we are live on port " + port);
   })
+});
