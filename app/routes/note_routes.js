c90ae1b9b6660e22670d27809e7e96ff599ca361
Jacob Spade <spade.jacob@Yahoo.com>
2019-04-18 11:35:16 -0700

    created edit and delete crud functionality

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,50 @@
+let ObjectID = require('mongodb').ObjectID
+
 module.exports = (app, db) => {
+
+  app.get('/notes/:id', (req,res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)};
+    db.db().collection('notes').findOne(details, (err, item) => {
+      if(err) {
+        res.send({
+          'error': 'an error happended'
+        })
+      } else {
+        res.send(item)
+      }
+    })
+  })
+
+  app.delete('/notes/:id', (req,res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)};
+    db.db().collection('notes').remove(details, (err, item) => {
+      if(err) {
+        res.send({
+          'error': 'an error happended'
+        })
+      } else {
+        res.send('note ' + id + ' deleted')
+      }
+    })
+  })
+
+  app.put('/notes/:id', (req,res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)};
+    const note = {text: req.body.body, title: req.body.title}
+    db.db().collection('notes').update(details, note, (err, item) => {
+      if(err) {
+        res.send({
+          'error': 'an error happended'
+        })
+      } else {
+        res.send(item)
+      }
+    })
+  })
+
   app.post('/notes', (req, res) => {
     const note = {text: req.body.body, title: req.body.title}
     db.db().collection('notes').insert(note, (err, result) => {
