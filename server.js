62875240a9eb48013c55b07f9a2ba1604c965c87
Jacob Spade <spade.jacob@Yahoo.com>
2019-04-18 10:01:00 -0700

    added mongo client connection

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const port = 5000;
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-MongoClient.connect("mongodb://jacob1:jacob1@ds139896.mlab.com:39896/notable", {useNewUrlParser: true});
+MongoClient.connect(db.url, (err, database) => {
+  if(err) {
+    return console.log(err)
+  } else {
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+      console.log("we are live on port " + port);
+    })
+  }
+});
 
 app.post('/notes', (req, res) => {
     const note = {text: req.body.body, title: req.body.title}
